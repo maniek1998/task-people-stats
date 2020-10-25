1bc6599d631780bcb491d4fefdac0febb912b1e8
maniek1998 <marcin1998119@gmail.com>
2020-10-25 21:40:44 +0100

    2commit

--- src/PeopleStats.java
+++ src/PeopleStats.java
@@ -1,2 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PeopleStats {
+    private final List<Person> people;
+
+    public PeopleStats(Path inputFilePath) {
+        try {
+            people = Files.lines(inputFilePath)
+                    .map(line -> line.split("\t"))
+                    .map(chunks -> new Person(chunks[0], chunks[1]))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 }
