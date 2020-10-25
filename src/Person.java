3a0a33222f8c3367334f03d17ba3a3312f59975f
maniek1998 <marcin1998119@gmail.com>
2020-10-25 21:37:22 +0100

    1 commit

---
+++ src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    private final String firstName;
+
+    public Person(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
